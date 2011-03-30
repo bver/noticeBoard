8b5e59ec60fe332a55a42017e90f6a411d5d9c34
Pavel Suchmann <pavel@suchmann.cz>
2011-03-30 21:28:08 +0200

    origin

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+UjsTest::Application.config.secret_token = 'e8b3fa91c285bea13b7202afc437917a4e5984bd32046c3f3620cd5cc3f52768fcbfe09820588f2cb8ab4d05315260043cbec46f377d2b0f8aa41515f0c29920'
