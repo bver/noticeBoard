6ad6a5b2b71de59ab5bd5525dde2fb0b1a938e79
Pavel Suchmann <pavel@suchmann.cz>
2011-04-06 23:54:46 +0200

    adding links to _menu

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,12 @@
 class ApplicationController < ActionController::Base
   before_filter :authenticate_user! unless Rails.env.test?
+  before_filter :boards
 
   protect_from_forgery
+
+  protected
+
+  def boards
+     @boards = Board.all
+  end
 end
