7e566bb842f5c314dfb775a90f6e6a177bb7712f
Pavel Suchmann <pavel@suchmann.cz>
2011-04-09 18:19:20 +0200

    change helper

--- app/models/change.rb
+++ app/models/change.rb
@@ -1,4 +1,15 @@
 class Change < ActiveRecord::Base
   belongs_to :note
   belongs_to :user
+
+  @@sense_values = [ :created, :finished, :cancelled, :accepted, :rejected, :assigned, :commented, :set_priority, :set_status ]
+
+  def sense
+     self.meaning.nil? ? nil : @@sense_values[self.meaning]
+  end
+
+  def sense= sym
+    self.meaning = sym.nil? ? nil : @@sense_values.index( sym )
+  end
+  
 end
--- test/unit/change_test.rb
+++ test/unit/change_test.rb
@@ -1,8 +1,52 @@
 require 'test_helper'
 
 class ChangeTest < ActiveSupport::TestCase
-  # Replace this with your real tests.
-  test "the truth" do
-    assert true
+  
+  test "sense helper fw" do
+    c = changes(:one)
+    c.meaning = nil
+    assert_equal nil, c.sense
+    c.meaning = 0
+    assert_equal :created, c.sense
+    c.meaning = 1
+    assert_equal :finished, c.sense
+    c.meaning = 2
+    assert_equal :cancelled, c.sense
+    c.meaning = 3
+    assert_equal :accepted, c.sense
+    c.meaning = 4
+    assert_equal :rejected, c.sense
+    c.meaning = 5
+    assert_equal :assigned, c.sense
+    c.meaning = 6
+    assert_equal :commented, c.sense
+    c.meaning = 7
+    assert_equal :set_priority, c.sense
+    c.meaning = 8
+    assert_equal :set_status, c.sense
+  end
+
+  test "sense helper bw" do
+    c = changes(:one)
+    c.sense = nil
+    assert_equal nil, c.meaning
+    c.sense = :created
+    assert_equal 0, c.meaning
+    c.sense = :finished
+    assert_equal 1, c.meaning
+    c.sense = :cancelled
+    assert_equal 2, c.meaning
+    c.sense = :accepted
+    assert_equal 3, c.meaning
+    c.sense = :rejected
+    assert_equal 4, c.meaning
+    c.sense = :assigned
+    assert_equal 5, c.meaning
+    c.sense = :commented
+    assert_equal 6, c.meaning
+    c.sense = :set_priority
+    assert_equal 7, c.meaning
+    c.sense = :set_status
+    assert_equal 8, c.meaning
   end
 end
