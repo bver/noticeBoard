13fd607ae5ab35df38a92d9499bc3b52cf98579e
Pavel Suchmann <pavel@suchmann.cz>
2011-04-19 20:16:32 +0200

    cosmetic, root route

--- config/routes.rb
+++ config/routes.rb
@@ -57,7 +57,7 @@ UjsTest::Application.routes.draw do
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
   
-  root :to => "boards#index"
+  root :to => "notes#index"
 
   # See how all your routes lay out with "rake routes"
 
